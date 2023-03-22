e38a16682da9806c2647296c79e312737fc3a162
Jorge Galvan <galvanjorgerdz@protonmail.com>
2023-03-22 17:47:13 -0600

    Revert "Se agrega conexion con la base de datos mediante mongoose"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import httpServer from "./config/httpServer.js";
 import "./config/env.js";
-import connectDB from "./config/db.js";
 
 const startFunc = () => {
-  connectDB(process.env.MONGO_URL);
   httpServer.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
   });
